3b8a32647ce86b1c15693b4768da5e4dc51b1b91
Marcin Czerniawski <253063@student.pwr.edu.pl>
2020-03-22 22:49:31 +0100

    22.03 po pliku array2d

--- main.cpp
+++ main.cpp
@@ -6,19 +6,34 @@ using namespace std;
 #include <ctime>
 #include "MinesweeperBoard.h"
 #include "MSBoardTextView.h"
-int main()
-{
-    srand( time( NULL ) );
-    MinesweeperBoard x(10,10,EASY);
+#include "Array2D.h"
+int main() {
+    srand(time(NULL));
+    MinesweeperBoard x(5, 10, DEBUG);
     //x.Debug_Display();
-    cout<<x.getMineCount()<<" ";
+    cout << x.getMineCount() << " ";
     //x.toggleFlag(3,4);
     //x.revealField(1,1);
     //cout<<" "<<x.getMineCount()<<" ";
     //cout<<x.countMines(1,1);
     MSBoardTextView view(x);
     view.display();
-    x.revealField(3,4);
-    x.revealField(1,2);
+    x.revealField(3, 4);
+    x.revealField(1, 2);
     view.display();
+    x.hasFlag(0, 0);
+    x.hasFlag(5, 10);
+    x.hasFlag(10000, 10000);
+    x.revealField(0, 0);
+    x.revealField(5, 10);
+    x.revealField(10000, 10000);
+    x.getFieldInfo(0,0);
+    x.getFieldInfo(5,10);
+    x.getFieldInfo(10000,10000);
+    x.countMines(0,0);
+    x.countMines(5,10);
+    x.countMines(10000,10000);
+    x.toggleFlag(0,0);
+    x.toggleFlag(5,10);
+    x.toggleFlag(10000,10000);
 }
