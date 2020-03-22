6abbb6318439861e9edf2364d3b27169829293d0
Marcin Czerniawski <253063@student.pwr.edu.pl>
2020-03-22 13:51:01 +0100

    22.03

--- main.cpp
+++ main.cpp
@@ -9,9 +9,9 @@ using namespace std;
 int main()
 {
     srand( time( NULL ) );
-    MinesweeperBoard x(10,10,HARD);
+    MinesweeperBoard x(10,10,EASY);
     //x.Debug_Display();
-   // cout<<x.getMineCount()<<" ";
+    cout<<x.getMineCount()<<" ";
     //x.toggleFlag(3,4);
     //x.revealField(1,1);
     //cout<<" "<<x.getMineCount()<<" ";
